5fb3b9b72517984f25961f645f4226284c32e6b6
Brad <bradculberson@gmail.com>
2021-03-01 10:31:13 -0700

    Add Service User, Schema, Grants

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "snowflake_svc_public_key" {
+    value = tls_private_key.svc_key.public_key_pem
+}
+
+output "snowflake_svc_private_key" {
+    value = tls_private_key.svc_key.private_key_pem
+}
